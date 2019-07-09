42cdab4bb82f2966e6df0e993a640a99fdf26333
Don Petersen <don@donpetersen.net>
2019-07-09 14:10:57 -0700

    Attempt map/blank map. Still doesn't work.

--- modules/postgresql/main.tf
+++ modules/postgresql/main.tf
@@ -21,6 +21,8 @@ locals {
     enabled  = "${var.ip_configuration}"
     disabled = "${map()}"
   }
+
+  tf_dependency_labels = "${var.peering_completed ? map("tf_dependency", var.peering_completed) : map()}"
 }
 
 resource "google_sql_database_instance" "default" {
@@ -46,7 +48,7 @@ resource "google_sql_database_instance" "default" {
     // Define a label to force a dependency to the creation of the network peering.
     // Substitute this with a module dependency once the module is migrated to
     // Terraform 0.12
-    user_labels = "${var.peering_completed ? merge(map("tf_dependency", var.peering_completed), var.user_labels) : var.user_labels}"
+    user_labels = "${merge(tf_dependency_labels, var.user_labels)}"
 
     location_preference {
       zone = "${var.region}-${var.zone}"
